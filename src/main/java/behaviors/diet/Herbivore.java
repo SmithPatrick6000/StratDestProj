dc20fa3b0f07447a32f59608555c22d14e7d6b40
SmithPatrick6000c <patdousmi@gmail.com>
2026-07-21 08:29:30 -0400

    Implemented DietBehaviors

---
+++ src/main/java/behaviors/diet/Herbivore.java
@@ -0,0 +1,8 @@
+package main.java.behaviors.diet;
+
+public class Herbivore implements DietBehavior{
+
+    @Override
+    public void move() { System.out.println("I eat Fruits & Vegitables"); }
+    
+}
---
+++ src/main/java/behaviors/diet/Insectivore.java
@@ -0,0 +1,8 @@
+package main.java.behaviors.diet;
+
+public class Insectivore implements DietBehavior{
+
+    @Override
+    public void diet() { System.out.println("I eat bugs"); }
+    
+}
